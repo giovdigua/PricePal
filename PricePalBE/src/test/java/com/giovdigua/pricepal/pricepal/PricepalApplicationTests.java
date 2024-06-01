c92a02e0d5d4b1e33d1dbe5d142532042771819a
Giovanni DI GUARDO <g.diguardo@cineca.it>
2024-06-01 12:35:15 +0200

    PRIMO COMMIT

---
+++ PricePalBE/src/test/java/com/giovdigua/pricepal/pricepal/PricepalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giovdigua.pricepal.pricepal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PricepalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
